e9a5bfaa3c504b98fbb1808bff1c841de8113d13
geod <geodgit@duck.com>
2026-02-06 17:37:46 +0100

    chore: enforce consistent code style using black, ruff, and isort

--- src/keneth/class_resolver/__init__.py
+++ src/keneth/class_resolver/__init__.py
@@ -1,3 +1,5 @@
+"""Provide a class resolver for dynamic class loading and resolution."""
+
 from .class_resolver import ClassResolver
 
 __all__ = ["ClassResolver"]
--- src/keneth/class_resolver/class_resolver.py
+++ src/keneth/class_resolver/class_resolver.py
@@ -1,26 +1,51 @@
 import importlib
 import pkgutil
 from abc import ABC
+from types import ModuleType
 
 from keneth.di_contracts import ServiceInterface
 
 
 class ClassResolver(ServiceInterface):
-    def __init__(self):
+    """ClassResolver service.
+
+    Responsible for dynamically loading modules
+    and resolving classes that are subclasses of a given base class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the ClassResolver with an empty list of modules."""
         self.modules = []
 
-    def __load_module__(self, package):
+    def __load_module(self, package: ModuleType) -> None:
+        """Load a module and its submodules from a given package.
+
+        Args:
+            package (ModuleType): The package to load modules from.
+        """
         for _, module_name, _ in pkgutil.walk_packages(
             package.__path__, package.__name__ + "."
         ):
             module = importlib.import_module(module_name)
             self.modules.append(module)
 
-    def load_modules(self, packages):
-        for package in packages:
-            self.__load_module__(package)
+    def load_modules(self, packages: list[ModuleType]) -> None:
+        """Load modules from a list of packages.
 
-    def resolve(self, base_class: ABC) -> list[ABC]:
+        Args:
+            packages (list[ModuleType]): The list of packages to load modules from.
+        """
+        for package in packages:
+            self.__load_module(package)
+
+    def resolve(self, base_class: type[ABC]) -> list[type[ABC]]:
+        """Resolve and return all subclasses of a given base class.
+
+        Args:
+            base_class (type[ABC]): The base class to find subclasses of.
+        Returns:
+            list[type[ABC]]: A list of subclasses of the given base class.
+        """
         subclasses = []
         for module in self.modules:
             for attribute_name in dir(module):
